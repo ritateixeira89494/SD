9957216f243f7e4a11bced60464521f6e5f45233
Mapc1 <marcocosta579@gmail.com>
2021-12-26 16:48:48 +0000

    Add TaggedConnection and create a simple server and client

--- src/main/java/com/sd/apps/Client.java
+++ src/main/java/com/sd/apps/Client.java
@@ -1,5 +1,29 @@
 package com.sd.apps;
 
+import java.net.Socket;
+import java.util.Scanner;
+
+import com.sd.net.TaggedConnection;
+import com.sd.net.TaggedConnection.Frame;
+
 public class Client {
-    
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", Integer.parseInt(args[0]));
+        TaggedConnection c = new TaggedConnection(socket);
+
+        Scanner scan = new Scanner(System.in);
+        boolean on = true;
+        while(on) {
+            String msg = scan.nextLine();
+
+            Frame f = new Frame(0,msg.getBytes());
+            c.send(f);
+            if(msg.equals("FYN")) {
+                System.out.println(msg + " sent. Closing connection");
+                on = false;
+            }
+        }
+        c.close();
+        scan.close();
+    }
 }
--- src/main/java/com/sd/apps/Server.java
+++ src/main/java/com/sd/apps/Server.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.sd.net.TaggedConnection;
+import com.sd.net.TaggedConnection.Frame;
+
 public class Server {
     public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
@@ -12,6 +15,26 @@ public class Server {
 
         while(true) {
             Socket s = ss.accept();
+            TaggedConnection c = new TaggedConnection(s);
+
+            Runnable r = () -> {
+                try {
+                    boolean on = true;
+                    while(on) {
+                        Frame f = c.receive();
+                        String msg = new String(f.data);
+                        if(msg.equals("FYN")) {
+                            System.out.println(msg + " received. Closing connection...");
+                            on = false;
+                            c.close();
+                        } else {
+                            System.out.println("Tag: " + f.tag + " Data: " + new String(f.data));
+                        }
+                    }
+                } catch (Exception e) {}
+            };
+            Thread t = new Thread(r);
+            t.start();
         }
     }
 }
